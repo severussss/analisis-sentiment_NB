e763953193d86a1c4e9d7c5f79ef0c87da5d881a
Severussss <farhanraldifan10@gmail.com>
2026-02-15 13:48:08 +0700

    first commit

--- stream_sentiment.py
+++ stream_sentiment.py
@@ -1,10 +1,8 @@
 import pandas as pd
-
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix, classification_report
-
 from imblearn.over_sampling import SMOTE
 
 
